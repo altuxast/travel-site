31d44a2d6ab52d94b9ee5ec1f5aa9d44ddf31820
George <drcageorge@gmail.com>
2019-12-18 21:52:35 +1100

    Installed web-dev-server package, added hot-loading, auto-refreshing

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,16 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app') // returns absolute path
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0',
+        before: function (app, server) {
+            server._watch('./app/**/*.html')
+        },
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
